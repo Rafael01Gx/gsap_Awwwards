5a5cd666307fdb413de0c271b9d7e09685aa7a58
Rafael Moraes <rafael_junio_moraes@hotmail.com>
2025-06-08 23:00:10 -0300

    refactor: enhance hero component with video handling and styling improvements

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+@Inject(PLATFORM_ID) private plataform_Id!: Object ; 
+@ViewChild('nextVideoRef') nextVideoRef!: ElementRef;
 
+  currentIndex = signal(1);
+  hasClicked = signal(false);
+  isLoading = signal(true);
+  loadedVideos = signal(0);
+  totalVideos = 4;
+  upcomingVideoIndex = ()=> (this.currentIndex() % this.totalVideos) + 1
+  
+handleMiniVdClick(){
+  this.hasClicked.set(true)
+  this.currentIndex.set(this.upcomingVideoIndex()); 
+}
+getVideoSrc(index:number): string {  
+  return `videos/hero-${index}.mp4`
+}  
+handleVideoLoad(){
+  if(isPlatformBrowser(this.plataform_Id)){
+  this.loadedVideos.update(prev => prev + 1)}
+}
 }
