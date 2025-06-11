2ca38f17cb8c115b122e1723302706d9dcdd835e
RafaelGx <rafael_junio_moraes@hotmail.com>
2025-06-11 12:55:12 -0300

    chore: upgrade Angular dependencies to version 20 and add GSAP for animations
    - Updated Angular packages from version 19 to 20 in package.json.
    - Replaced deprecated imports in app.config.server.ts for server rendering.
    - Refactored hero.component.html for improved structure and fixed a typo in muted attribute.
    - Enhanced hero.component.ts to utilize GSAP for animations and improved video handling logic.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,37 +1,81 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, ElementRef, Inject, PLATFORM_ID, signal, ViewChild } from '@angular/core';
-import { ButtonComponent } from "../button/button.component";
+import {
+  Component,
+  effect,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 import { svgIcons } from '../../icons/icons';
+import { gsap } from 'gsap';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 
 @Component({
   selector: 'app-hero',
   imports: [ButtonComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-@Inject(PLATFORM_ID) private plataform_Id!: Object ; 
-@ViewChild('nextVideoRef') nextVideoRef!: ElementRef;
-  icon=signal(svgIcons)
+  @ViewChild('currentVideoRef', { static: true })
+  currentVideoRef!: ElementRef;
+  @ViewChild('nextVideoRef', { static: true })
+  nextVideoRef!: ElementRef;
+  icon = signal(svgIcons);
   currentIndex = signal(1);
   hasClicked = signal(false);
   isLoading = signal(true);
   loadedVideos = signal(0);
   totalVideos = 4;
-  upcomingVideoIndex = ()=> (this.currentIndex() % this.totalVideos) + 1
-  
+  viewReady = signal(false);
+  upcomingVideoIndex = () => (this.currentIndex() % this.totalVideos) + 1;
+  private animationContext?: gsap.Context;
 
+  handleMiniVdClick() {
+    this.hasClicked.set(true);
+    this.currentIndex.set(this.upcomingVideoIndex());
+  }
+  getVideoSrc(index: number): string {
+    return `videos/hero-${index}.mp4`;
+  }
+  handleVideoLoad() {
+    if (isPlatformBrowser(this.platform_Id)) {
+      this.loadedVideos.update((prev) => prev + 1);
+    }
+  }
+  constructor(@Inject(PLATFORM_ID) private platform_Id: Object) {
+    if (isPlatformBrowser(this.platform_Id)) {
+      gsap.registerPlugin(MotionPathPlugin);
+      effect(() => {
+        if (this.hasClicked() && this.currentIndex()) {
+          this.animationContext?.revert();
+          this.animationContext = gsap.context(() => {
+            gsap.set(this.nextVideoRef.nativeElement, {
+              visibility: 'visible',
+            });
 
-handleMiniVdClick(){
-  this.hasClicked.set(true)
-  this.currentIndex.set(this.upcomingVideoIndex()); 
-}
-getVideoSrc(index:number): string {  
-  return `videos/hero-${index}.mp4`
-}  
-handleVideoLoad(){
-  if(isPlatformBrowser(this.plataform_Id)){
-  this.loadedVideos.update(prev => prev + 1)}
-}
+            gsap.to(this.nextVideoRef.nativeElement, {
+              transformOrigin: 'center center',
+              scale: 1,
+              width: '100%',
+              height: '100%',
+              duration: 1,
+              ease: 'power1.inOut',
+              onStart: () => this.nextVideoRef.nativeElement.play(),
+            });
 
+            gsap.from(this.currentVideoRef.nativeElement, {
+              transformOrigin: 'center center',
+              scale: 0,
+              duration: 1.5,
+              ease: 'power1.inOut',
+            });
+          });
+        }
+      });
+    }
+  }
 }
